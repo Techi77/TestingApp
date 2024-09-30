c8952cdba16ea76cbec5c8e4809b394ab455910b
Kate Rossinskaya <rossinskaya98@gmail.com>
2024-09-30 15:18:59 +0300

    clean code

--- app/src/main/java/com/example/testingapp/MainActivity.kt
+++ app/src/main/java/com/example/testingapp/MainActivity.kt
@@ -3,44 +3,31 @@ package com.example.testingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.relocation.BringIntoViewRequester
-import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material.icons.rounded.KeyboardArrowUp
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -50,7 +37,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.testingapp.ui.theme.TestingAppTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -58,7 +44,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestingAppTheme {
                 ExpandableList(
-                    modifier = Modifier.fillMaxWidth(),
                     fixedContent = {
                         Text(
                             text = "Смогу ли я просматривать видео с камер, находясь в другом городе или за границей?",
@@ -67,12 +52,6 @@ class MainActivity : ComponentActivity() {
                     },
                     expandableContentInList = List(10) { index ->
                         index.toString()
-                        /*{
-                            Text(
-                                text = index.toString(),
-                                modifier = Modifier.fillMaxWidth(),
-                            )
-                        }*/
                     }
                 )
             }
@@ -80,91 +59,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun ExpandableListV2(
-    modifier: Modifier = Modifier,
-    onExpandOrCollapseClick: (() -> Unit)? = null,
-    fixedContent: @Composable () -> Unit,
-    expandableContentInList: List<String>,
-) {
-    var expanded by remember { mutableStateOf(false) }//TODO верни как было
-    val bringIntoViewRequester = remember { BringIntoViewRequester() }
-    Card(
-        modifier = modifier.bringIntoViewRequester(bringIntoViewRequester),
-        shape = RoundedCornerShape(24.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(start = 16.dp, top = 12.dp, end = 12.dp, bottom = 12.dp)
-                .fillMaxWidth()
-        ) {
-            Row {
-                Box(
-                    modifier = Modifier
-                        .padding(end = 12.dp)
-                        .align(Alignment.CenterVertically)
-                        .weight(1f)
-                ) {
-                    fixedContent()
-                }
-                Crossfade(
-                    expanded,
-                    animationSpec = tween(durationMillis = AnimationDurationMillis),
-                    label = "ExpandableCardArrowAnimation"
-                ) {
-                    val icon =
-                        if (it) Icons.Rounded.KeyboardArrowUp else Icons.Rounded.KeyboardArrowDown
-                    Icon(
-                        imageVector = icon,
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .size(36.dp)
-                            .clickable {
-                                expanded = !(expanded)
-                                onExpandOrCollapseClick?.invoke()
-                            }
-                            .background(Color.Gray)
-                            .padding(8.dp),
-                        contentDescription = "",
-                        tint = Color.Red,
-                    )
-                }
-            }
-            LazyColumn {
-                if (expanded) {
-                    itemsIndexed(
-                        items = expandableContentInList,
-                        key = { _, item -> item }) { index, item ->
-                        var visible by remember { mutableStateOf(!expanded) }
-                        LaunchedEffect(Unit) {
-                            delay(100L * index)
-                            visible = !visible
-                        }
-                        AnimatedVisibility(
-                            visible = visible,
-                            enter = fadeIn() + expandVertically(),
-                            exit = fadeOut() + shrinkVertically(),
-                            modifier = Modifier.animateItem()
-                        ) {
-                            Text(item)
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ExpandableList(
-    modifier: Modifier = Modifier,
     onExpandOrCollapseClick: (() -> Unit)? = null,
     fixedContent: @Composable () -> Unit,
     expandableContentInList: List<String>,
 ) {
-    val expanded = remember { mutableStateListOf(false, false, false) }//TODO верни как было
+    val expanded = remember { mutableStateListOf(false, false, false) }
     var speedVariant by remember { mutableIntStateOf(0) }
     var ratioVariant by remember { mutableIntStateOf(0) }
     var animationVariant by remember { mutableIntStateOf(0) }
@@ -208,10 +109,10 @@ fun ExpandableList(
                 Button(onClick = { animationVariant = 1 }) {
                     Text("tween")
                 }
-                Text(if(animationVariant == 0) "spring" else "tween")
+                Text(if (animationVariant == 0) "spring" else "tween")
             }
         }
-        expanded.forEachIndexed{index, value ->
+        expanded.forEachIndexed { index, value ->
             item {
                 Row {
                     Box(
@@ -257,11 +158,14 @@ fun ExpandableList(
                             .animateItem(
                                 fadeInSpec =
                                 when (animationVariant) {
-                                     0 -> spring(
+                                    0 -> spring(
                                         dampingRatio = setAnimationRatio(ratioVariant),
                                         stiffness = setAnimationSpeed(speedVariant)
                                     )
-                                    else -> tween(durationMillis = setAnimationSpeed(speedVariant).toInt(), easing = FastOutSlowInEasing)
+                                    else -> tween(
+                                        durationMillis = setAnimationSpeed(speedVariant).toInt(),
+                                        easing = FastOutSlowInEasing
+                                    )
                                 },
                                 placementSpec =
                                 when (animationVariant) {
@@ -269,7 +173,11 @@ fun ExpandableList(
                                         dampingRatio = setAnimationRatio(ratioVariant),
                                         stiffness = setAnimationSpeed(speedVariant)
                                     )
-                                    else -> tween(durationMillis = setAnimationSpeed(speedVariant).toInt(), easing = FastOutSlowInEasing)
+
+                                    else -> tween(
+                                        durationMillis = setAnimationSpeed(speedVariant).toInt(),
+                                        easing = FastOutSlowInEasing
+                                    )
                                 }
                             )
                     ) {
@@ -281,13 +189,13 @@ fun ExpandableList(
     }
 }
 
-private fun setAnimationRatio(ratioIndex:Int) = when (ratioIndex) {
+private fun setAnimationRatio(ratioIndex: Int) = when (ratioIndex) {
     0 -> Spring.DampingRatioLowBouncy
     1 -> Spring.DampingRatioMediumBouncy
     else -> Spring.DampingRatioHighBouncy
 }
 
-private fun setAnimationSpeed(speedIndex:Int) = when (speedIndex) {
+private fun setAnimationSpeed(speedIndex: Int) = when (speedIndex) {
     0 -> Spring.StiffnessVeryLow
     1 -> Spring.StiffnessMedium
     else -> Spring.StiffnessHigh
@@ -300,7 +208,6 @@ private const val AnimationDurationMillis = 200
 internal fun ExpandableCardPreview() {
     TestingAppTheme {
         ExpandableList(
-            modifier = Modifier.fillMaxWidth(),
             fixedContent = {
                 Text(
                     text = "Смогу ли я просматривать видео с камер, находясь в другом городе или за границей?",
@@ -320,7 +227,6 @@ internal fun ExpandableCardPreview() {
 internal fun ExpandableCardSkeletonPreview() {
     TestingAppTheme {
         ExpandableList(
-            modifier = Modifier.fillMaxWidth(),
             fixedContent = {},
             expandableContentInList = listOf()
         )
